8c7a95242c2cfe6eba42d3f60ffbf1de5927e507
Elhadj Diallo <dlloelh@gmail.com>
2020-05-28 20:16:17 -0700

    Quick Updates

---
+++ Pointer.h
@@ -0,0 +1,21 @@
+//
+// Created by Elhadj Diallo on 10/7/19.
+//
+
+#ifndef CSS342_POINTER_H
+#define CSS342_POINTER_H
+
+#include <iostream>
+class Pointer {
+    int x=5;
+/*
+ * fgbo n
+    jbdgjbi
+*/
+ public:
+    void  TestPointers();
+
+};
+
+
+#endif //CSS342_POINTER_H
